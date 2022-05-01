7a90deb92e9e027e9253fbdc9ee8fbfda7a16f1e
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-01 11:49:35 +0300

    Updated AbstractDao

--- model/src/main/java/com/piatnitsa/dao/AbstractDao.java
+++ model/src/main/java/com/piatnitsa/dao/AbstractDao.java
@@ -4,6 +4,7 @@ import com.piatnitsa.exception.DaoException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -14,7 +15,8 @@ import java.util.Optional;
  * @author Vlad Piatnitsa
  * @version 1.0
  */
-public abstract class AbstractDao<T> implements CRDDao<T>{
+public abstract class AbstractDao<T> implements CRDDao<T> {
+
     @PersistenceContext
     protected EntityManager entityManager;
     protected final Class<T> entityType;
@@ -23,16 +25,27 @@ public abstract class AbstractDao<T> implements CRDDao<T>{
         this.entityType = entityType;
     }
 
+    @Override
+    public Optional<T> getById(long id) {
+        return Optional.of(entityManager.find(entityType, id));
+    }
+
+    @Override
+    public List<T> getAll() {
+        return entityManager.createQuery("select e from " + entityType.getSimpleName() + " e", entityType)
+                .getResultList();
+    }
+
+    @Override
+    public T insert(T item) throws DaoException {
+        entityManager.persist(item);
+        return item;
+    }
 
     @Override
     public void removeById(long id) throws DaoException {
         T entity = entityManager.find(entityType, id);
         entityManager.remove(entity);
-    }
-
-    @Override
-    public Optional<T> getById(long id) {
-        return Optional.of(entityManager.find(entityType, id));
     }
 
 }
