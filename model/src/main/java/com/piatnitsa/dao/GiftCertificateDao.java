fb28858c0b9a8c8b97b41e56066eb73b1f4a6448
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-02 12:42:35 +0300

    Added interfaces with special dao methods

---
+++ model/src/main/java/com/piatnitsa/dao/GiftCertificateDao.java
@@ -0,0 +1,23 @@
+package com.piatnitsa.dao;
+
+import com.piatnitsa.entity.GiftCertificate;
+import com.piatnitsa.exception.DaoException;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This interface describes abstract behavior for working with <code>gift_certificate</code> table in database.
+ *
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
+public interface GiftCertificateDao extends CRUDDao<GiftCertificate> {
+
+    /**
+     * Method for getting a list of {@link GiftCertificate} by specific parameters.
+     * @param params request parameters from URL.
+     * @return {@link List} of {@link GiftCertificate}.
+     */
+    List<GiftCertificate> getWithFilter(Map<String, String> params);
+}
---
+++ model/src/main/java/com/piatnitsa/dao/TagDao.java
@@ -0,0 +1,31 @@
+package com.piatnitsa.dao;
+
+import com.piatnitsa.entity.Tag;
+import com.piatnitsa.exception.DaoException;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * This interface describes abstract behavior for working with <code>tag</code> table in database.
+ *
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
+public interface TagDao extends CRDDao<Tag>{
+
+    /**
+     * Retrieves an {@link Tag} entity by its name.
+     * @param name entity name.
+     * @return an {@link Tag} entity.
+     */
+    Optional<Tag> getByName(String name);
+
+    /**
+     * Method for getting a list of {@link Tag} by specific parameters.
+     * @param params request parameters from URL.
+     * @return {@link List} of {@link Tag}.
+     */
+    List<Tag> getWithFilter(Map<String, String> params);
+}
