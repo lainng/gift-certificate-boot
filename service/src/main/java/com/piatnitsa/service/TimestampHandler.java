f54c792835a56019eb2dcf71536bdc08b98397b3
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-02 23:23:55 +0300

    Added timestamp handler for certificate create & update field

---
+++ service/src/main/java/com/piatnitsa/service/TimestampHandler.java
@@ -0,0 +1,24 @@
+package com.piatnitsa.service;
+
+import com.piatnitsa.entity.GiftCertificate;
+
+import java.time.LocalDateTime;
+
+/**
+ * This class represents the time handler needed to update
+ * the timestamp of the {@link GiftCertificate}.
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ * @see GiftCertificate
+ */
+public class TimestampHandler {
+
+    /**
+     * Returns the current timestamp in ISO 8601 format.
+     * @return current timestamp of string type.
+     */
+    public static String getCurrentTimestamp() {
+        LocalDateTime dateTime = LocalDateTime.now();
+        return dateTime.toString();
+    }
+}
