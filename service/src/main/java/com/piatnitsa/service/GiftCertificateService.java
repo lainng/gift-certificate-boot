52df2315631e75677efdb1f48b53efd5e27012db
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-02 23:14:36 +0300

    Added interfaces with specific service operations

---
+++ service/src/main/java/com/piatnitsa/service/GiftCertificateService.java
@@ -0,0 +1,23 @@
+package com.piatnitsa.service;
+
+import com.piatnitsa.entity.GiftCertificate;
+import com.piatnitsa.exception.IncorrectParameterException;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This interface describes abstract behavior for working with {@link GiftCertificate} objects.
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
+public interface GiftCertificateService extends CRUDService<GiftCertificate> {
+
+    /**
+     * Method for getting a list of {@link GiftCertificate} by specific parameters.
+     * @param params request parameters from URL.
+     * @return {@link List} of {@link GiftCertificate}.
+     * @throws IncorrectParameterException if specified parameters contains incorrect parameters.
+     */
+    List<GiftCertificate> doFilter(Map<String, String> params) throws IncorrectParameterException;
+}
---
+++ service/src/main/java/com/piatnitsa/service/TagService.java
@@ -0,0 +1,23 @@
+package com.piatnitsa.service;
+
+import com.piatnitsa.entity.Tag;
+import com.piatnitsa.exception.IncorrectParameterException;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This interface describes abstract behavior for working with {@link Tag} objects.
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
+public interface TagService extends CRDService<Tag> {
+
+    /**
+     * Method for getting a list of {@link Tag} by specific parameters.
+     * @param params request parameters from URL.
+     * @return {@link List} of {@link Tag}.
+     * @throws IncorrectParameterException if specified parameters contains incorrect parameters.
+     */
+    List<Tag> doFilter(Map<String, String> params) throws IncorrectParameterException;
+}
