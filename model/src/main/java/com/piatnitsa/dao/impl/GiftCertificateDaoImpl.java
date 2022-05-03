d9b4a6c21464a11cf777ef242b356ae922b2a6bf
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-03 17:27:08 +0300

    Fixed incorrect display of the list of certificates in the absence of one of the filter parameters.

--- model/src/main/java/com/piatnitsa/dao/impl/GiftCertificateDaoImpl.java
+++ model/src/main/java/com/piatnitsa/dao/impl/GiftCertificateDaoImpl.java
@@ -37,45 +37,56 @@ public class GiftCertificateDaoImpl extends AbstractDao<GiftCertificate> impleme
         Root<GiftCertificate> root = criteriaQuery.from(GiftCertificate.class);
 
         List<Predicate> predicates = new ArrayList<>(params.size());
-        predicates.add(criteriaBuilder.like(
-                criteriaBuilder.lower(root.get("name")),
-                "%" + params.get("name").toLowerCase() + "%")
-        );
-        predicates.add(criteriaBuilder.like(
-                criteriaBuilder.lower(root.get("description")),
-                "%" + params.get("description").toLowerCase() + "%")
-        );
-        predicates.add(criteriaBuilder.like(
-                criteriaBuilder.lower(root.join("tags").get("name")),
-                "%" + params.get("tag_name").toLowerCase() + "%")
-        );
+        if (params.containsKey("name")) {
+            predicates.add(criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("name")),
+                    "%" + params.get("name").toLowerCase() + "%")
+            );
+        }
+        if (params.containsKey("description")) {
+            predicates.add(criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("description")),
+                    "%" + params.get("description").toLowerCase() + "%")
+            );
+        }
+        if (params.containsKey("tag_name")) {
+            predicates.add(criteriaBuilder.like(
+                    criteriaBuilder.lower(root.join("tags").get("name")),
+                    "%" + params.get("tag_name").toLowerCase() + "%")
+            );
+        }
         criteriaQuery.select(root).where(predicates.toArray(new Predicate[]{}));
 
-        String nameSortType = params.get("name_sort").toLowerCase();
-        switch (nameSortType) {
-            case "asc": {
-                criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
-                break;
+        if (params.containsKey("name_sort")) {
+            String nameSortType = params.get("name_sort").toLowerCase();
+            switch (nameSortType) {
+                case "asc": {
+                    criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
+                    break;
+                }
+                case "desc": {
+                    criteriaQuery.orderBy(criteriaBuilder.desc(root.get("name")));
+                    break;
+                }
             }
-            case "desc": {
-                criteriaQuery.orderBy(criteriaBuilder.desc(root.get("name")));
-                break;
+        }
+        if (params.containsKey("date_sort")) {
+            String dateSortType = params.get("date_sort").toLowerCase();
+            switch (dateSortType) {
+                case "asc": {
+                    criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
+                    break;
+                }
+                case "desc": {
+                    criteriaQuery.orderBy(criteriaBuilder.desc(root.get("name")));
+                    break;
+                }
             }
         }
 
-        String dateSortType = params.get("date_sort").toLowerCase();
-        switch (dateSortType) {
-            case "asc": {
-                criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
-                break;
-            }
-            case "desc": {
-                criteriaQuery.orderBy(criteriaBuilder.desc(root.get("name")));
-                break;
-            }
-        }
         return entityManager.createQuery(criteriaQuery)
                 .getResultStream()
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
