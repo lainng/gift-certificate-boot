de67af2b436fd2dd0e72bf93b35a4f7af438093c
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-04 20:36:56 +0300

    Added validator for filter sorting type

---
+++ service/src/main/java/com/piatnitsa/validator/FilterParameterValidator.java
@@ -0,0 +1,26 @@
+package com.piatnitsa.validator;
+
+import com.piatnitsa.exception.IncorrectParameterException;
+import com.piatnitsa.exception.IncorrectParameterMessageCodes;
+
+/**
+ * This class provides a validator for entity filter parameters.
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ */
+public class FilterParameterValidator {
+    private static final String ASCENDING = "ASC";
+    private static final String DESCENDING = "DESC";
+
+    /**
+     * Validates sort order types.
+     * @param value sort order type.
+     * @throws IncorrectParameterException if sort type not equal "ASC" or "DESC".
+     */
+    public static void validateSortType(String value) throws IncorrectParameterException {
+        if (value == null
+                || (!value.equalsIgnoreCase(ASCENDING) && !value.equalsIgnoreCase(DESCENDING))) {
+            throw new IncorrectParameterException(IncorrectParameterMessageCodes.BAD_SORTING_TYPE);
+        }
+    }
+}
