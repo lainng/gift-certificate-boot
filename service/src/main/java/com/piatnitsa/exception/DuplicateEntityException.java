1027f7033a657e91586ce6918b82811f4b67a0ea
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-05 21:57:52 +0300

    Added new exception

---
+++ service/src/main/java/com/piatnitsa/exception/DuplicateEntityException.java
@@ -0,0 +1,20 @@
+package com.piatnitsa.exception;
+
+public class DuplicateEntityException extends RuntimeException{
+
+    public DuplicateEntityException() {
+    }
+
+    public DuplicateEntityException(String messageCode) {
+        super(messageCode);
+    }
+
+    public DuplicateEntityException(String messageCode, Throwable cause) {
+        super(messageCode, cause);
+    }
+
+    public DuplicateEntityException(Throwable cause) {
+        super(cause);
+    }
+
+}
