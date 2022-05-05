2ec90913777359f08fe2943b1a402f3e569fb6dd
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-05 22:12:38 +0300

    Added new exception

---
+++ service/src/main/java/com/piatnitsa/exception/IncorrectParameterException.java
@@ -0,0 +1,29 @@
+package com.piatnitsa.exception;
+
+/**
+ * This class represents the exception that is thrown when the validation of entity parameters fails.
+ *
+ * @author Vlad Piatnitsa
+ * @version 1.0
+ * @see com.piatnitsa.validator.IdentifiableValidator
+ * @see com.piatnitsa.validator.GiftCertificateValidator
+ * @see com.piatnitsa.validator.TagValidator
+ * @see com.piatnitsa.validator.FilterParameterValidator
+ */
+public class IncorrectParameterException extends RuntimeException {
+
+    public IncorrectParameterException() {
+    }
+
+    public IncorrectParameterException(String messageCode) {
+        super(messageCode);
+    }
+
+    public IncorrectParameterException(String messageCode, Throwable cause) {
+        super(messageCode, cause);
+    }
+
+    public IncorrectParameterException(Throwable cause) {
+        super(cause);
+    }
+}
