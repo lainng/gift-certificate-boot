f0e38291f9c1ce090605d6d39a346a6413b5f2b3
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-06 16:48:06 +0300

    Added queryCreator field

--- model/src/main/java/com/piatnitsa/dao/impl/GiftCertificateDaoImpl.java
+++ model/src/main/java/com/piatnitsa/dao/impl/GiftCertificateDaoImpl.java
@@ -2,26 +2,25 @@ package com.piatnitsa.dao.impl;
 
 import com.piatnitsa.dao.AbstractDao;
 import com.piatnitsa.dao.GiftCertificateDao;
+import com.piatnitsa.dao.creator.QueryCreator;
 import com.piatnitsa.entity.GiftCertificate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Component
+@Repository
 public class GiftCertificateDaoImpl extends AbstractDao<GiftCertificate> implements GiftCertificateDao {
+    private final QueryCreator<GiftCertificate> queryCreator;
 
     @Autowired
-    public GiftCertificateDaoImpl() {
+    public GiftCertificateDaoImpl(QueryCreator<GiftCertificate> queryCreator) {
         super(GiftCertificate.class);
+        this.queryCreator = queryCreator;
     }
 
     @Override
@@ -32,58 +31,10 @@ public class GiftCertificateDaoImpl extends AbstractDao<GiftCertificate> impleme
 
     @Override
     public List<GiftCertificate> getWithFilter(Map<String, String> params) {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<GiftCertificate> criteriaQuery = criteriaBuilder.createQuery(GiftCertificate.class);
-        Root<GiftCertificate> root = criteriaQuery.from(GiftCertificate.class);
-
-        List<Predicate> predicates = new ArrayList<>(params.size());
-        if (params.containsKey("name")) {
-            predicates.add(criteriaBuilder.like(
-                    criteriaBuilder.lower(root.get("name")),
-                    "%" + params.get("name").toLowerCase() + "%")
-            );
-        }
-        if (params.containsKey("description")) {
-            predicates.add(criteriaBuilder.like(
-                    criteriaBuilder.lower(root.get("description")),
-                    "%" + params.get("description").toLowerCase() + "%")
-            );
-        }
-        if (params.containsKey("tag_name")) {
-            predicates.add(criteriaBuilder.like(
-                    criteriaBuilder.lower(root.join("tags").get("name")),
-                    "%" + params.get("tag_name").toLowerCase() + "%")
-            );
-        }
-        criteriaQuery.select(root).where(predicates.toArray(new Predicate[]{}));
-
-        if (params.containsKey("name_sort")) {
-            String nameSortType = params.get("name_sort").toLowerCase();
-            switch (nameSortType) {
-                case "asc": {
-                    criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
-                    break;
-                }
-                case "desc": {
-                    criteriaQuery.orderBy(criteriaBuilder.desc(root.get("name")));
-                    break;
-                }
-            }
-        }
-        if (params.containsKey("date_sort")) {
-            String dateSortType = params.get("date_sort").toLowerCase();
-            switch (dateSortType) {
-                case "asc": {
-                    criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
-                    break;
-                }
-                case "desc": {
-                    criteriaQuery.orderBy(criteriaBuilder.desc(root.get("name")));
-                    break;
-                }
-            }
-        }
-
+        CriteriaQuery<GiftCertificate> criteriaQuery = queryCreator.createFilteringGetQuery(
+                params,
+                entityManager.getCriteriaBuilder()
+        );
         return entityManager.createQuery(criteriaQuery)
                 .getResultStream()
                 .distinct()
--- model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
+++ model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
@@ -2,15 +2,12 @@ package com.piatnitsa.dao.impl;
 
 import com.piatnitsa.dao.AbstractDao;
 import com.piatnitsa.dao.TagDao;
+import com.piatnitsa.dao.creator.QueryCreator;
 import com.piatnitsa.entity.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -18,11 +15,13 @@ import java.util.stream.Collectors;
 
 @Repository
 public class TagDaoImpl extends AbstractDao<Tag> implements TagDao {
+    private final QueryCreator<Tag> queryCreator;
     private static final String QUERY_SELECT_BY_NAME = "select t from Tag t where t.name = :name";
 
     @Autowired
-    public TagDaoImpl() {
+    public TagDaoImpl(QueryCreator<Tag> queryCreator) {
         super(Tag.class);
+        this.queryCreator = queryCreator;
     }
 
     @Override
@@ -35,30 +34,10 @@ public class TagDaoImpl extends AbstractDao<Tag> implements TagDao {
 
     @Override
     public List<Tag> getWithFilter(Map<String, String> params) {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Tag> criteriaQuery = criteriaBuilder.createQuery(Tag.class);
-        Root<Tag> root = criteriaQuery.from(Tag.class);
-
-        List<Predicate> predicates = new ArrayList<>(1);
-        predicates.add(criteriaBuilder.like(
-                criteriaBuilder.lower(root.get("name")),
-                "%" + params.get("tag_name").toLowerCase() + "%")
+        CriteriaQuery<Tag> criteriaQuery = queryCreator.createFilteringGetQuery(
+                params,
+                entityManager.getCriteriaBuilder()
         );
-
-        criteriaQuery.select(root).where(predicates.toArray(new Predicate[0]));
-        if (params.containsKey("tag_name_sort")) {
-            String sortType = params.get("tag_name_sort").toLowerCase();
-            switch (sortType) {
-                case "asc": {
-                    criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
-                    break;
-                }
-                case "desc": {
-                    criteriaQuery.orderBy(criteriaBuilder.desc(root.get("name")));
-                    break;
-                }
-            }
-        }
         return entityManager.createQuery(criteriaQuery)
                 .getResultStream()
                 .collect(Collectors.toList());
