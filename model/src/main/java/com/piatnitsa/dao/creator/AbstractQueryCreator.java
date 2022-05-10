e5b7675f51cbedf3cfb470ca23b9cf5913fe493f
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-10 12:45:05 +0300

    Updated methods for working with orders and predicates

--- model/src/main/java/com/piatnitsa/dao/creator/AbstractQueryCreator.java
+++ model/src/main/java/com/piatnitsa/dao/creator/AbstractQueryCreator.java
@@ -1,25 +1,34 @@
 package com.piatnitsa.dao.creator;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.Map;
 
 public abstract class AbstractQueryCreator<T> {
 
-    protected Predicate addTagNamePredicate(Map<String, String> params,
-                                         CriteriaBuilder criteriaBuilder,
-                                         Root<T> root) {
+    protected Predicate addLikePredicate(CriteriaBuilder criteriaBuilder,
+                                         Expression<String> expression,
+                                         String parameter) {
         return criteriaBuilder.like(
-                criteriaBuilder.lower(root.get("name")),
-                "%" + params.get("tag_name").toLowerCase() + "%");
+                criteriaBuilder.lower(expression),
+                "%" + parameter.toLowerCase() + "%");
     }
 
-    protected Predicate addNamePredicate(Map<String, String> params,
-                                         CriteriaBuilder criteriaBuilder,
-                                         Root<T> root) {
-        return criteriaBuilder.like(
-                criteriaBuilder.lower(root.get("name")),
-                "%" + params.get("name").toLowerCase() + "%");
+    protected Order addOrder(CriteriaBuilder criteriaBuilder,
+                             Expression<String> expression,
+                             String sortType) {
+        Order order = null;
+        switch (sortType.toLowerCase()) {
+            case "asc": {
+                order = criteriaBuilder.asc(expression);
+                break;
+            }
+            case "desc": {
+                order = criteriaBuilder.desc(expression);
+                break;
+            }
+        }
+        return order;
     }
 }
