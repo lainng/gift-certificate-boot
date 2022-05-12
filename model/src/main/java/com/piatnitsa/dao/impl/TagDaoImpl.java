84b448a8a855555acd00709f4267a466c1663d53
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-12 21:52:11 +0300

    Fixed a bug when the entityManager did not find a tag with an existing name

--- model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
+++ model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
@@ -2,16 +2,20 @@ package com.piatnitsa.dao.impl;
 
 import com.piatnitsa.dao.AbstractDao;
 import com.piatnitsa.dao.TagDao;
+import com.piatnitsa.dao.creator.FilterParameter;
 import com.piatnitsa.dao.creator.QueryCreator;
 import com.piatnitsa.entity.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class TagDaoImpl extends AbstractDao<Tag> implements TagDao {
-    private static final String QUERY_SELECT_BY_NAME = "select t from Tag t where t.name = :name";
 
     @Autowired
     public TagDaoImpl(QueryCreator<Tag> queryCreator) {
@@ -20,8 +24,10 @@ public class TagDaoImpl extends AbstractDao<Tag> implements TagDao {
 
     @Override
     public Optional<Tag> getByName(String name) {
-        return entityManager.createQuery(QUERY_SELECT_BY_NAME, Tag.class)
-                .setParameter("name", name)
+        Map<String, String> paramMap = Collections.singletonMap(FilterParameter.TAG_NAME, name);
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tag> criteriaQuery = queryCreator.createFilteringGetQuery(paramMap, criteriaBuilder);
+        return entityManager.createQuery(criteriaQuery)
                 .getResultStream()
                 .findFirst();
     }
