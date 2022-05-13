9d0a86035f3b4c56fa8d0cfaf08e9b1774c284ab
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-13 21:54:11 +0300

    Method refactoring

--- model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
+++ model/src/main/java/com/piatnitsa/dao/impl/TagDaoImpl.java
@@ -7,11 +7,11 @@ import com.piatnitsa.dao.creator.QueryCreator;
 import com.piatnitsa.entity.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import java.util.Collections;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -24,7 +24,8 @@ public class TagDaoImpl extends AbstractDao<Tag> implements TagDao {
 
     @Override
     public Optional<Tag> getByName(String name) {
-        Map<String, String> paramMap = Collections.singletonMap(FilterParameter.TAG_NAME, name);
+        MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
+        paramMap.add(FilterParameter.TAG_NAME, name);
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Tag> criteriaQuery = queryCreator.createFilteringGetQuery(paramMap, criteriaBuilder);
         return entityManager.createQuery(criteriaQuery)
