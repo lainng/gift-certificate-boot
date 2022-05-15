66c110a1c19b9a6cc1f2ac4dd17bd2854961ed1a
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-15 17:15:07 +0300

    Added new entities

---
+++ model/src/main/java/com/piatnitsa/entity/User.java
@@ -0,0 +1,53 @@
+package com.piatnitsa.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("User{");
+        builder.append("id=").append(id)
+                .append(", name='").append(name)
+                .append('}');
+        return builder.toString();
+    }
+}
