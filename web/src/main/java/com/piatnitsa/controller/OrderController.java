4dcea1f6f66fbda1c39bdf2604870476bae62c9d
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-16 12:55:05 +0300

    Added new endpoint (creating an order)

--- web/src/main/java/com/piatnitsa/controller/OrderController.java
+++ web/src/main/java/com/piatnitsa/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.piatnitsa.controller;
 
+import com.piatnitsa.dto.OrderDto;
+import com.piatnitsa.dto.converter.DtoConverter;
 import com.piatnitsa.entity.Order;
 import com.piatnitsa.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +14,23 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final DtoConverter<OrderDto, Order> orderDtoConverter;
 
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, DtoConverter<OrderDto, Order> orderDtoConverter) {
         this.orderService = orderService;
+        this.orderDtoConverter = orderDtoConverter;
     }
 
     @GetMapping("/users/{userId}")
     @ResponseStatus(HttpStatus.OK)
     public List<Order> ordersByUserId(@PathVariable long userId) {
         return orderService.getOrdersByUserId(userId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Order createOrder(@RequestBody OrderDto orderDto) {
+        return orderService.insert(orderDtoConverter.toEntity(orderDto));
     }
 }
