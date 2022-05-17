41f65629b23769f47c4be595f37202eecdb260f0
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-17 21:48:28 +0300

    Updated fields and add hateoas support

--- web/src/main/java/com/piatnitsa/dto/OrderDto.java
+++ web/src/main/java/com/piatnitsa/dto/OrderDto.java
@@ -1,22 +1,56 @@
 package com.piatnitsa.dto;
 
-public class OrderDto {
-    private long giftCertificateId;
-    private long userId;
+import com.piatnitsa.entity.GiftCertificate;
+import com.piatnitsa.entity.User;
+import org.springframework.hateoas.RepresentationModel;
 
-    public long getGiftCertificateId() {
-        return giftCertificateId;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderDto extends RepresentationModel<OrderDto> {
+    private long id;
+    private BigDecimal cost;
+    private LocalDateTime purchaseTime;
+    private GiftCertificate certificate;
+    private User user;
+
+    public long getId() {
+        return id;
     }
 
-    public void setGiftCertificateId(long giftCertificateId) {
-        this.giftCertificateId = giftCertificateId;
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public long getUserId() {
-        return userId;
+    public BigDecimal getCost() {
+        return cost;
     }
 
-    public void setUserId(long userId) {
-        this.userId = userId;
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    public LocalDateTime getPurchaseTime() {
+        return purchaseTime;
+    }
+
+    public void setPurchaseTime(LocalDateTime purchaseTime) {
+        this.purchaseTime = purchaseTime;
+    }
+
+    public GiftCertificate getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(GiftCertificate certificate) {
+        this.certificate = certificate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
