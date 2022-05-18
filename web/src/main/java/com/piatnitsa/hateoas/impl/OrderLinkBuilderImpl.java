90f8c01b2c3218e0a5037a1c3bb3159498373bdd
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-18 20:13:36 +0300

    Removed incorrect link to self

--- web/src/main/java/com/piatnitsa/hateoas/impl/OrderLinkBuilderImpl.java
+++ web/src/main/java/com/piatnitsa/hateoas/impl/OrderLinkBuilderImpl.java
@@ -1,18 +1,13 @@
 package com.piatnitsa.hateoas.impl;
 
-import com.piatnitsa.controller.OrderController;
 import com.piatnitsa.dto.GiftCertificateDto;
 import com.piatnitsa.dto.OrderDto;
 import com.piatnitsa.hateoas.LinkBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 @Component
 public class OrderLinkBuilderImpl implements LinkBuilder<OrderDto> {
-    private static final Class<OrderController> CONTROLLER_CLASS = OrderController.class;
     private final LinkBuilder<GiftCertificateDto> certificateDtoLinkBuilder;
 
     @Autowired
@@ -22,7 +17,6 @@ public class OrderLinkBuilderImpl implements LinkBuilder<OrderDto> {
 
     @Override
     public void buildLinks(OrderDto object) {
-        object.add(linkTo(methodOn(CONTROLLER_CLASS).ordersByUserId(object.getUser().getId())).withSelfRel());
         certificateDtoLinkBuilder.buildLinks(object.getCertificate());
     }
 }
