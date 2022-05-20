072dc925cdec2409d1ce85f880da75303d70c845
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-20 13:00:06 +0300

    Added pagination support

--- service/src/main/java/com/piatnitsa/service/OrderService.java
+++ service/src/main/java/com/piatnitsa/service/OrderService.java
@@ -5,5 +5,5 @@ import com.piatnitsa.entity.Order;
 import java.util.List;
 
 public interface OrderService extends CRDService<Order> {
-    List<Order> getOrdersByUserId (long userId);
+    List<Order> getOrdersByUserId (long userId, int page, int size);
 }
