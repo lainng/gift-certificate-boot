8a801d15f48de39d7a02674a4f79b99b4f48b85a
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-26 15:51:21 +0300

    Added test tables data script

---
+++ model/src/test/resources/fillingTagTable.sql
@@ -0,0 +1,5 @@
+insert into tag (id, tag_name) values (1, 'tagName1');
+insert into tag (id, tag_name) values (2, 'tagName3');
+insert into tag (id, tag_name) values (3, 'tagName5');
+insert into tag (id, tag_name) values (4, 'tagName4');
+insert into tag (id, tag_name) values (5, 'tagName2');
