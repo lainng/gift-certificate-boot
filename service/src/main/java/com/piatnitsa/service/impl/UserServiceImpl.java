084b590289e3219903ddcc920d72416e81ff4c04
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-05-29 22:18:04 +0300

    Updated user service methods

--- service/src/main/java/com/piatnitsa/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/piatnitsa/service/impl/UserServiceImpl.java
@@ -4,19 +4,32 @@ import com.piatnitsa.dao.UserDao;
 import com.piatnitsa.entity.User;
 import com.piatnitsa.service.AbstractService;
 import com.piatnitsa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
 
 @Component
 public class UserServiceImpl extends AbstractService<User> implements UserService {
-    private final UserDao userDao;
 
+    @Autowired
     public UserServiceImpl(UserDao userDao) {
         super(userDao);
-        this.userDao = userDao;
     }
 
     @Override
     public User insert(User entity) {
-        return userDao.insert(entity);
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeById(long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<User> doFilter(MultiValueMap<String, String> params, int page, int size) {
+        throw new UnsupportedOperationException();
     }
 }
