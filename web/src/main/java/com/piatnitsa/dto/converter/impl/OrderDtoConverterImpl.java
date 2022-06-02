cd90a17ba8997f9413d4e56f571ce06a6b6f6659
Vlad Piatnitsa <vpyatnitsa@icloud.com>
2022-06-02 20:00:41 +0300

    Updated method

--- web/src/main/java/com/piatnitsa/dto/converter/impl/OrderDtoConverterImpl.java
+++ web/src/main/java/com/piatnitsa/dto/converter/impl/OrderDtoConverterImpl.java
@@ -3,6 +3,7 @@ package com.piatnitsa.dto.converter.impl;
 import com.piatnitsa.dto.GiftCertificateDto;
 import com.piatnitsa.dto.OrderCreationDto;
 import com.piatnitsa.dto.OrderDto;
+import com.piatnitsa.dto.UserDto;
 import com.piatnitsa.dto.converter.DtoConverter;
 import com.piatnitsa.entity.GiftCertificate;
 import com.piatnitsa.entity.Order;
@@ -13,10 +14,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderDtoConverterImpl implements DtoConverter<OrderDto, Order> {
     private final DtoConverter<GiftCertificateDto, GiftCertificate> certificateDtoConverter;
+    private final DtoConverter<UserDto, User> userDtoConverter;
 
     @Autowired
-    public OrderDtoConverterImpl(DtoConverter<GiftCertificateDto, GiftCertificate> certificateDtoConverter) {
+    public OrderDtoConverterImpl(DtoConverter<GiftCertificateDto, GiftCertificate> certificateDtoConverter,
+                                 DtoConverter<UserDto, User> userDtoConverter) {
         this.certificateDtoConverter = certificateDtoConverter;
+        this.userDtoConverter = userDtoConverter;
     }
 
     @Override
@@ -43,7 +47,7 @@ public class OrderDtoConverterImpl implements DtoConverter<OrderDto, Order> {
         dto.setCost(entity.getCost());
         dto.setPurchaseTime(entity.getPurchaseTime());
         dto.setCertificate(certificateDtoConverter.toDto(entity.getCertificate()));
-        dto.setUser(entity.getUser());
+        dto.setUser(userDtoConverter.toDto(entity.getUser()));
 
         return dto;
     }
